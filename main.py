71b2942c5a29b6aa25b5132b0c2cf8e2e2a35a05
Jacek Dzbański <jacek17123@gmail.com>
2026-02-06 16:02:03 +0100

    Add numpy financial function to calculate future value and price raise of flat in 5 years

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-    
+import numpy as np
+import numpy_financial as npf
+
+
 if __name__ == '__main__':
     
     rate = 0.05
@@ -9,3 +12,9 @@ if __name__ == '__main__':
     rate_invest = 0.12
     all_periods = periods*12
     rate_invest /= freq
+
+pv_price = - np.around(npf.fv(rate, periods, 0, pv),2)
+
+price_change = -npf.fv(rate, np.arange(0, periods+1), 0, pv)
+
+print(pv_price)
