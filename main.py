0e7ddcacb075a6b267538aa6554520c39fb0e67f
Jacek Dzbański <jacek17123@gmail.com>
2026-02-06 15:58:49 +0100

    Add variables

---
+++ main.py
@@ -0,0 +1,11 @@
+    
+if __name__ == '__main__':
+    
+    rate = 0.05
+    periods = 5
+    pv = 120000
+
+    freq = 12
+    rate_invest = 0.12
+    all_periods = periods*12
+    rate_invest /= freq
